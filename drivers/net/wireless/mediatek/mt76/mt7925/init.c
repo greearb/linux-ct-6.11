a1de607cfdd6dbae143224e679a31e9d11de650e
Rory Little <rory@candelatech.com>
2025-06-23 17:42:29 -0700

    wifi: mt76: mt7925: Fix race for userspace ethtool stat querying
    Without this, the length of the stats can change immediately after being
    reported by the driver. This is because has_eht only is true after a
    scan has been completed to ensure that the reg domain supports eht.
    Instead, always report eht stats, even if they are not used by the
    reg domain.
    
    Signed-off-by: Rory Little <rory@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7925/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/init.c
@@ -223,6 +223,8 @@ int mt7925_register_device(struct mt792x_dev *dev)
 	dev->mt76.phy.priv = &dev->phy;
 	dev->mt76.tx_worker.fn = mt792x_tx_worker;
 
+	dev->could_eht = true;
+
 	INIT_DELAYED_WORK(&dev->pm.ps_work, mt792x_pm_power_save_work);
 	INIT_WORK(&dev->pm.wake_work, mt792x_pm_wake_work);
 	spin_lock_init(&dev->pm.wake.lock);
